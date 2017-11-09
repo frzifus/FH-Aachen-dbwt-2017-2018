05534d795946301d800bdbb625e1608a6739cb23
Frzifus <benne@klimlive.de>
2017-11-09 18:24:58 +0100

    added test model

---
+++ model/test.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+    "time"
+)
+
+//Test is just a test
+type Test struct {
+    ID        int       `schema:"-"`
+    Test      string    `schema:"test"`
+    CreatedAt time.Time `schema:"-"`
+    UpdatedAt time.Time `schema:"-"`
+}
