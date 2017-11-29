432223728fe72403a4bcbf178a7507c8d055b7cb
Frzifus <benne@klimlive.de>
2017-11-29 11:41:40 +0100

    added bootstrap 3.3.7

---
+++ static/scripts/custom.js
@@ -0,0 +1,15 @@
+// $("#btn_t_and_c").on('click', function(event) {
+//     var x =  $("#t_and_c").attr('value');
+
+//     if(x == 0) {
+//         x = 1;
+//         $("#t_and_c").prop( "checked", true );
+//         $("#t_and_c").attr('value', x);
+//         $(event.target).attr('class', 'btn btn-primary btn-block');
+//     } else {
+//         x = 0;
+//         $("#t_and_c").prop( "checked", false );
+//         $("#t_and_c").attr('value', x);
+//         $(event.target).attr('class', 'btn btn-secondary btn-block');
+//     }
+// });
