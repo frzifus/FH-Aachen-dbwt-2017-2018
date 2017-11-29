e271e9d204425dcf7446294f95dcfa3facbaf5c6
Frzifus <benne@klimlive.de>
2017-11-29 12:38:28 +0100

    update ingredient model

--- model/dbwt.go
+++ model/dbwt.go
@@ -7,43 +7,44 @@ import (
 
 // User - FH-Nutzer
 type User struct {
-	Nr           uint           `gorm:"column:nr;AUTO_INCREMENT;primary_key"`
-	Active       bool           `gorm:"column:active;default:true"`
-	Firstname    string         `gorm:"column:firstname;not null"`
-	Lastname     string         `gorm:"column:lastname;not null"`
-	Mail         string         `gorm:"column:mail;unique"`
-	Loginname    string         `gorm:"column:loginname"`
-	CreatedAt    time.Time      `gorm:"column:created_at;default:CURRENT_DATE"`
-	LastLogin    time.Time      `gorm:"column:last_login"`
-	Stretch      uint           `gorm:"column:stretch;not null"`
-	Algo         string         `gorm:"column:algo;not null"`
-	Salt         string         `gorm:"column:salt;type:varchar(32);not null"`
-	Hash         string         `gorm:"column:hash;type:varchar(64);not null"`
+	Nr        uint      `gorm:"column:nr;AUTO_INCREMENT;primary_key"`
+	Active    bool      `gorm:"column:active;default:true"`
+	Firstname string    `gorm:"column:firstname;not null"`
+	Lastname  string    `gorm:"column:lastname;not null"`
+	Mail      string    `gorm:"column:mail;unique"`
+	Loginname string    `gorm:"column:loginname"`
+	CreatedAt time.Time `gorm:"column:created_at;default:CURRENT_DATE"`
+	LastLogin time.Time `gorm:"column:last_login"`
+	Stretch   uint      `gorm:"column:stretch;not null"`
+	Algo      string    `gorm:"column:algo;not null"`
+	Salt      string    `gorm:"column:salt;type:varchar(32);not null"`
+	Hash      string    `gorm:"column:hash;type:varchar(64);not null"`
 }
 
 // Member - Fh-Angehoerige
 type Member struct {
 	Nr     uint `gorm:"column:nr;primary_key"`
-    User   User `gorm:"ForeignKey:UserNr;AssociationForeignKey:Nr"`
-    UserNr uint
+	User   User `gorm:"ForeignKey:UserNr;AssociationForeignKey:Nr"`
+	UserNr uint
 }
 
 // Guest - Gast
 type Guest struct {
-    Reason     string    `gorm:"column:reason;not null"`
+	Reason     string    `gorm:"column:reason;not null"`
 	ExpiryDate time.Time `gorm:"column:expiry_date;default:CURRENT_DATE"`
 	Nr         uint      `gorm:"column:nr;not null"`
 }
 
 // Student - Student
 type Student struct {
-	StudentId      int    `gorm:"column:student_id;not null"`
-	Course         string `gorm:"column:course;not null"`
-    Member         Member `gorm:"column:nr;ForeignKey:UserNr;AssociationForeignKey:Refer"`
-    MemberNr       uint
+	StudentID int    `gorm:"column:student_id;not null"`
+	Course    string `gorm:"column:course;not null"`
+	Member    Member `gorm:"column:nr;ForeignKey:UserNr;AssociationForeignKey:Refer"`
+	MemberNr  uint
 
-	Nr             uint  `schema:"nr"`
+	Nr uint `schema:"nr"`
 }
+
 //////////////////////////////////////////////
 // Mitarbeiter -
 type Mitarbeiter struct {
@@ -78,14 +79,14 @@ type Kategorie struct {
 
 // Produkt -
 type Product struct {
-	Id            int           `schema:"id"`
+	ID            int           `schema:"id"`
 	Beschreibung  string        `schema:"beschreibung"`
 	Vegetarisch   sql.NullInt64 `schema:"vegetarisch"`
 	Vegan         sql.NullInt64 `schema:"vegan"`
 	ProduktbildId int           `schema:"produktbildId"`
 	KategorieId   int           `schema:"kategorieId"`
+	Ingredients   []Ingredient  `gorm:"many2many:products_ingredients;"`
 }
-
 
 // Preis -
 type Preis struct {
@@ -95,25 +96,14 @@ type Preis struct {
 	Produkt           int64 `schema:"produkt"`
 }
 
-// Zutat
-type Zutat struct {
-	Id           int64          `schema:"id"`
-	Glutenfrei   sql.NullInt64  `schema:"glutenfrei"`
-	Bio          sql.NullInt64  `schema:"bio"`
-	Vegetarisch  sql.NullInt64  `schema:"vegetarisch"`
-	Vegan        sql.NullInt64  `schema:"vegan"`
-	Beschreibung sql.NullString `schema:"beschreibung"`
-	Name         string         `schema:"Name"`
+// Ingredient - Zutat
+type Ingredient struct {
+	ID          int64     `gorm:"column:id;AUTO_INCREMENT;primary_key"`
+	Glutenfree  bool      `gorm:"column:gluten_free;not null"`
+	Bio         bool      `gorm:"column:bio;not null"`
+	Vegetarian  bool      `gorm:"column:vegetarian;not null"`
+	Vegan       bool      `gorm:"column:vegan;not null"`
+	Description string    `gorm:"column:description;not null"`
+	Name        string    `gorm:"column:name;type:varchar(64)"`
+	Products    []Product `gorm:"many2many:products_ingredients;"`
 }
-
-// // TransProduktBestellung -
-// type TransProduktBestellung struct {
-//	Produktid int64 `schema:"produktid"`
-//	Bestellid int64 `schema:"bestellid"`
-// }
-
-// // TransProduktZutat -
-// type TransProduktZutat struct {
-//	ProduktId int64 `schema:"produktId"`
-//	ZutatId   int64 `schema:"zutatId"`
-// }
