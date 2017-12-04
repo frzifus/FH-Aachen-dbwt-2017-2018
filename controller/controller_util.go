e27b431bb26b2b38737d1cf4f95d6062ddd671b7
Frzifus <benne@klimlive.de>
2017-12-04 22:44:22 +0100

    controller_util has been extended by a role check

--- controller/controller_util.go
+++ controller/controller_util.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/gorilla/sessions"
 	"net/http"
+	"reflect"
 )
 
 func signedIn(r *http.Request, store sessions.Store) bool {
@@ -14,4 +15,16 @@ func signedIn(r *http.Request, store sessions.Store) bool {
 		return true
 	}
 	return false
+}
+
+func role(r *http.Request, store sessions.Store) string {
+	s, err := store.Get(r, "SessionOtherCookie")
+	if err != nil {
+		return ""
+	}
+	if _, ok := s.Values["role"]; ok {
+		r := reflect.ValueOf(s.Values["role"]).String()
+		return r
+	}
+	return ""
 }
