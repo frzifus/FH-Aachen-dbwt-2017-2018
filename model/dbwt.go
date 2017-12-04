5e7ab6a39fe1c588e1e9a5feb873ef64c04df8b7
Frzifus <benne@klimlive.de>
2017-12-04 18:21:50 +0100

    go fmt dbwt.go

--- model/dbwt.go
+++ model/dbwt.go
@@ -19,33 +19,33 @@ type User struct {
 	Algo      string    `gorm:"column:algo;not null"`
 	Salt      string    `gorm:"column:salt;type:varchar(32);not null"`
 	Hash      string    `gorm:"column:hash;type:varchar(64);not null"`
-    MemberID  uint
-    GuestID   uint
-    Orders    []Order   `gorm:"ForeignKey:UserID;AssociationForeignKey:ID"`
+	MemberID  uint
+	GuestID   uint
+	Orders    []Order `gorm:"ForeignKey:UserID;AssociationForeignKey:ID"`
 }
 
 // Member - Fh-Angehoerige
 type Member struct {
-	ID     uint `gorm:"column:id"`
-    User   User `gorm:"ForeignKey:ID;AssociationForeignKey:ID"`
+	ID   uint `gorm:"column:id"`
+	User User `gorm:"ForeignKey:ID;AssociationForeignKey:ID"`
 
-    StudentID  uint
-    EmployeeID uint
+	StudentID  uint
+	EmployeeID uint
 }
 
 // Guest - Gast
 type Guest struct {
-    ID         uint      `gorm:"column:id;not null"`
+	ID         uint      `gorm:"column:id;not null"`
 	Reason     string    `gorm:"column:reason;not null"`
 	ExpiryDate time.Time `gorm:"column:expiry_date;default:CURRENT_DATE"`
 }
 
 // Student - Student
 type Student struct {
-    ID        uint 
+	ID        uint
 	StudentID uint   `gorm:"column:student_id;not null"`
 	Course    string `gorm:"column:course;not null"`
-    Member    Member `gorm:"ForeignKey:StudentID;AssociationForeignKe1y:ID"`
+	Member    Member `gorm:"ForeignKey:StudentID;AssociationForeignKe1y:ID"`
 }
 
 // Employee - Mitarbeiter
@@ -53,24 +53,24 @@ type Employee struct {
 	ID          uint   `gorm:"id;not null"`
 	PhoneNumber int    `gorm:"column:phone_number;not null"`
 	Office      string `gorm:"office;not null"`
-    Member      Member `gorm:"ForeignKey:EmployeeID;AssociationForeignKe1y:ID"`
+	Member      Member `gorm:"ForeignKey:EmployeeID;AssociationForeignKe1y:ID"`
 }
 
 // Order - Bestellung
 type Order struct {
 	ID      uint      `gorm:"column:ID;AUTO_INCREMENT;primary_key"`
 	Time    time.Time `gorm:"column:time;default:CURRENT_DATE"`
-	UserID  uint  
-    Product []Product `gorm:"many2many:orders_products;"`
+	UserID  uint
+	Product []Product `gorm:"many2many:orders_products;"`
 }
 
 // Image - Bild
 type Image struct {
-	ID      uint            `gorm:"column:id;AUTO_INCREMENT;primary_key"`
-	Bindata []byte          `gorm:"column:blob_data"`
-	Alttext sql.NullString  `gorm:"column:alttext"`
-	Title   sql.NullString  `gorm:"column:title"`
-	Caption string          `gorm:"column:caption;not null"`
+	ID      uint           `gorm:"column:id;AUTO_INCREMENT;primary_key"`
+	Bindata []byte         `gorm:"column:blob_data"`
+	Alttext sql.NullString `gorm:"column:alttext"`
+	Title   sql.NullString `gorm:"column:title"`
+	Caption string         `gorm:"column:caption;not null"`
 }
 
 // Category - Kategorie
@@ -78,8 +78,8 @@ type Category struct {
 	ID              uint   `gorm:"column:id;AUTO_INCREMENT;primary_key"`
 	Designation     string `gorm:"column:designation;not null"`
 	UpperCategoryID uint
-    UpperCategory   *Category `gorm:"ForeignKey:UpperCaterogyID"`
-	ImageID         uint  
+	UpperCategory   *Category `gorm:"ForeignKey:UpperCaterogyID"`
+	ImageID         uint
 }
 
 // Product - Produkt
@@ -87,11 +87,11 @@ type Product struct {
 	ID          int    `gorm:"column:id;AUTO_INCREMENT;primary_key"`
 	Name        string `gorm:"column:name;not null"`
 	Description string `gorm:"column:description;not null"`
-	ImageID     int    
-	CategoryID  int    
+	ImageID     int
+	CategoryID  int
 	PriceID     uint
 	Price       Price        `gorm:"ForeignKey:PriceID"`
-    Orders      []Product    `gorm:"many2many:orders_products;"`
+	Orders      []Product    `gorm:"many2many:orders_products;"`
 	Ingredients []Ingredient `gorm:"many2many:products_ingredients;"`
 }
 
@@ -106,8 +106,8 @@ type Price struct {
 // Ingredient - Zutat
 type Ingredient struct {
 	ID          uint      `gorm:"column:id;AUTO_INCREMENT;primary_key"`
-    Name        string    `gorm:"column:name;type:varchar(64);not null"`
-    Description string    `gorm:"column:description;not null"`
+	Name        string    `gorm:"column:name;type:varchar(64);not null"`
+	Description string    `gorm:"column:description;not null"`
 	Glutenfree  bool      `gorm:"column:gluten_free;not null"`
 	Bio         bool      `gorm:"column:bio;not null"`
 	Vegetarian  bool      `gorm:"column:vegetarian;not null"`
