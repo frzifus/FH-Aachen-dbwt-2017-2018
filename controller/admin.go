54bc5f5ba4e2d577e4efb0fc856a197b049a251d
Frzifus <benne@klimlive.de>
2018-01-17 14:33:49 +0100

    pass over new registrations and active users to the new admin template

--- controller/admin.go
+++ controller/admin.go
@@ -4,6 +4,7 @@ import (
 	"github.com/frzifus/dbwt/model"
 	"github.com/gernest/utron/controller"
 	"net/http"
+	"time"
 )
 
 type admin struct {
@@ -22,6 +23,19 @@ func NewAdmin() controller.Controller {
 func (a *admin) Admin() {
 	if !a.isAdmin() {
 		a.Ctx.Redirect("/", http.StatusFound)
+	}
+	a.Ctx.Data["signedIn"] = signedIn(a.Ctx.Request(), a.Ctx.SessionStore)
+
+	layout := "2006-01-02 15:04:05"
+	str := "0001-01-01 00:00:00"
+	t, _ := time.Parse(layout, str)
+
+	users := []*model.User{}
+	if err := a.Ctx.DB.Where("last_login != ?", t).Find(&users).Error; err == nil {
+		a.Ctx.Data["userLogins"] = users
+	}
+	if err := a.Ctx.DB.Where("last_login = ?", t).Find(&users).Error; err == nil {
+		a.Ctx.Data["newRegistrations"] = users
 	}
 
 	a.Ctx.Template = "admin/admin"
